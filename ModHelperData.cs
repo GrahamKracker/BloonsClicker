f721b64f4a3c1a9f21d8e6ee1f96d73acce717d4
GrahamKracker <86011515+GrahamKracker@users.noreply.github.com>
2023-11-17 11:30:05 -0500

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -3,7 +3,7 @@
 public static class ModHelperData
 {
     public const string Name = "BloonsClicker";
-    public const string Description = "";
+    public const string Description = "Allows you to upgrade your cursor to pop Bloons. Thanks to Rubby (https://www.youtube.com/@RubbyBTD) for all thhe artwork";
     public const string Version = "1.0.0";
     public const string RepoOwner = "GrahamKracker";
     public const string RepoName = "BloonsClicker";
