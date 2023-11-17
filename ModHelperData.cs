8761d3a6580606f10eaa69400c4dca063c26064a
GrahamKracker <86011515+GrahamKracker@users.noreply.github.com>
2023-11-17 11:28:09 -0500

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -7,5 +7,5 @@ public static class ModHelperData
     public const string Version = "1.0.0";
     public const string RepoOwner = "GrahamKracker";
     public const string RepoName = "BloonsClicker";
-    public const string WorksOnVersion = "36.3";
+    public const string WorksOnVersion = "39.2";
 }
